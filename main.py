5fa48664be265200dd87499231c3aee37383cfcb
horstrs <horst.marcelo@gmail.com>
2025-09-28 20:19:37 -0300

    Reporting done

--- main.py
+++ main.py
@@ -1,14 +1,29 @@
-from stats import count_words, count_characters
+from stats import count_words, count_characters, create_sorted_list_of_dicts
 
 def get_book_text(file_path):
     with open(file_path) as file:
         return file.read()
 
+def print_all_in_new_line(list_of_dict):
+    for entry in list_of_dict:
+        if not entry["char"].isalpha():
+            continue
+        print(f"{entry["char"]}: {entry["num"]}")
+
+def print_report(words, characters):
+    print("============ BOOKBOT ============")
+    print("Analyzing book found at books/frankenstein.txt...")
+    print("----------- Word Count ----------")
+    print(f"Found {words} total words")
+    print("--------- Character Count -------")
+    print_all_in_new_line(characters)
+    print("============= END ===============")
+
 def main():
     book_text = get_book_text("books/frankenstein.txt").lower()
     num_words = count_words(book_text)
-    num_characters = count_characters(book_text)
-    print(f"Found {num_words} total words")
-    print(num_characters)
+    character_dict = count_characters(book_text)
+    sorted_list_of_dicts = create_sorted_list_of_dicts(character_dict)
+    print_report(num_words, sorted_list_of_dicts)    
 
 main()
--- stats.py
+++ stats.py
@@ -9,3 +9,18 @@ def count_characters(text):
     for character in text:
         character_dictionary[character] += 1
     return character_dictionary
+
+def sort_on_num(dict):
+    return dict["num"]
+
+def create_sorted_list_of_dicts(dictionary_with_all_characters):
+    list_of_dicts = []
+    for key in dictionary_with_all_characters:
+        character_dict_entry = {"char": "a", "num": 0}
+        character_dict_entry["char"] = key
+        character_dict_entry["num"] = dictionary_with_all_characters[key]
+        list_of_dicts.append(character_dict_entry)
+
+    list_of_dicts.sort(reverse=True, key=sort_on_num)
+
+    return list_of_dicts
