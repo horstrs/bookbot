595e191ecaac2754841485efd2eccbb028eb82f7
horstrs <horst.marcelo@gmail.com>
2025-09-28 18:43:43 -0300

    Added stats file

--- main.py
+++ main.py
@@ -1,8 +1,11 @@
+from stats import count_words
+
 def get_book_text(file_path):
     with open(file_path) as file:
         return file.read()
     
 def main():
-    print(get_book_text("books/frankenstein.txt"))
+    num_words = count_words(get_book_text("books/frankenstein.txt"))
+    print(f"Found {num_words} total words")
 
 main()
---
+++ stats.py
@@ -0,0 +1,3 @@
+def count_words(text):
+    words_split = text.split()
+    return len(words_split)
